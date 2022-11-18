ff5821e36e8eaaf05b88d7475397733d2264a27c
tau123 <tajuttt512ctg@gmail.com>
2022-11-18 19:29:50 +0600

    new chat

--- component/Messenger.js
+++ component/Messenger.js
@@ -1,38 +1,36 @@
+import Script from 'next/script';
 import React from 'react';
-import { MessengerChat } from "react-messenger-chat-plugin";
+
 
 const Messenger = () => {
 
       return (
             <div>
-                  <MessengerChat
-                        pageId="100072578250427"
-                        language="en_US"
-                        themeColor={"#fff"}
-                        bottomSpacing={300}
-                        loggedInGreeting="loggedInGreeting"
-                        loggedOutGreeting="loggedOutGreeting"
-                        greetingDialogDisplay={"show"}
-                        debugMode={true}
-                        onMessengerShow={() => {
-                              console.log("onMessengerShow");
-                        }}
-                        onMessengerHide={() => {
-                              console.log("onMessengerHide");
-                        }}
-                        onMessengerDialogShow={() => {
-                              console.log("onMessengerDialogShow");
-                        }}
-                        onMessengerDialogHide={() => {
-                              console.log("onMessengerDialogHide");
-                        }}
-                        onMessengerMounted={() => {
-                              console.log("onMessengerMounted");
-                        }}
-                        onMessengerLoad={() => {
-                              console.log("onMessengerLoad");
-                        }}
-                  />
+                  <div id="fb-root"></div>
+
+                  <div id="fb-customer-chat" className="fb-customerchat"></div>
+                  <Script strategy="lazyOnload">
+                        {`
+                  var chatbox = document.getElementById('fb-customer-chat');
+                  chatbox.setAttribute("page_id", "YOUR_PAGE_ID");
+                  chatbox.setAttribute("attribution", "biz_inbox");
+            
+                  window.fbAsyncInit = function() {
+                    FB.init({
+                      xfbml            : true,
+                      version          : 'v12.0'
+                    });
+                  };
+            
+                  (function(d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    if (d.getElementById(id)) return;
+                    js = d.createElement(s); js.id = id;
+                    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+                    fjs.parentNode.insertBefore(js, fjs);
+                  }(document, 'script', 'facebook-jssdk'));
+              `}
+                  </Script>
             </div>
       );
 };
